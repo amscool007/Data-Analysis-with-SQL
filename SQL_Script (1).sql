4a8615d7350ab16f22f0ec047583c973a57219c2
Amit <51916702+amscool007@users.noreply.github.com>
2021-02-20 12:32:17 +0000

    Add files via upload

---
+++ SQL_Script (1).sql
@@ -0,0 +1,28 @@
+ 
+ select * from ProductInventory;
+
+
+
+
+SELECT *
+FROM ProductSubcategory;
+
+
+
+SELECT *
+FROM ProductCategory;
+
+
+
+SELECT ProductKey
+	,ProductSubcategoryKey
+	,ProductName
+	,StandardCost
+	,ListPrice
+	,SupplierId
+FROM product;
+
+
+ 
+
+select  ReorderPoint,* from product 
